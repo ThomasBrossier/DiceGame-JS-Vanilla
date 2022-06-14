a30411183404c24e80590f574ac971d666d1322b
ThomasBrossier <98547958+ThomasBrossier@users.noreply.github.com>
2022-06-14 17:40:27 +0200

    Add Dice & Roll dice functionnality

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,19 @@
-function newgame(){
+// Fonction qui retourne un nombre aléatoire de 1 à 6
+const rollDice = ()=>{
+    const nombre = Math.trunc((Math.random()*6) + 1);
+    return nombre
+}
+
+
+const initplayers = ()=>{
 let player1 = {}
 let player2 = {}
 player1.name = prompt("Veuillez saisir le nom du joueur 1");
-if(player1.name === ""){
+if(player1.name === "" || player1.name === null){
     player1.name = "Joueur1";
 }
 player2.name = prompt("Veuillez saisir le nom du joueur 2");
-if(player2.name === ""){
+if(player2.name === "" || player2.name === null){
     player2.name = "Joueur2";
 }
 namePlayer1 = document.getElementById("player1");
@@ -14,3 +21,4 @@ namePlayer2 = document.getElementById("player2");
 namePlayer1.innerText = player1.name;
 namePlayer2.innerText = player2.name
 }
+
--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 //creation des variables representant des éléments du DOM
+let currentUser = 1;
 let newgamebutton = document.querySelector(".newgame");
-let playButtons = document.getElementsByClassName("playButtons")
+let img = document.getElementById("dice");
 let [scoreP1, scoreP2] = document.getElementsByClassName("played");
-let [currentP1, currentP2] = document.getElementsByClassName("current")
+let [currentP1, currentP2] = document.getElementsByClassName("current");
+let playButtons = document.getElementsByClassName("playButtons")
+let played = document.getElementsByClassName("played")
+let playButton = document.getElementById("play")
 //creation des Events
 newgamebutton.addEventListener('click',()=>{
     newgamebutton.style.display = "none";
@@ -12,6 +16,20 @@ newgamebutton.addEventListener('click',()=>{
     scoreP1.innerText = 0;
     scoreP2.innerText = 0;
     currentP1.innerText = 0;
-    currentP1.innerText = 0;
-    newgame();
+    currentP2.innerText = 0;
+    //initplayers();
+})
+
+playButton.addEventListener('click',()=>{
+    let dice = rollDice()
+    img.src = "./assets/images/de-"+ dice +".gif" ;
+    if(currentUser == 1){
+        scoreP2.innerHTML = 0
+        scoreP1.innerHTML = dice;
+        currentUser = 2
+    }else{
+        scoreP1.innerHTML = 0
+        scoreP2.innerHTML = dice;
+        currentUser = 1 ;
+    }
 })
